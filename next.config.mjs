dcd9b19e3e5ce4a2fa8e8bd12bfd607867a0e30f
Ayush <asharma7588@gmail.com>
2025-03-09 15:17:52 +0530

    push changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
     domains: ["localhost"], // Add the domain you want to allow for external images
   },
   reactStrictMode: false, // Disable StrictMode
-  eslint:{
-    ignoreDuringBuildsx:true
-  },
+  // eslint:{
+  //   ignoreDuringBuildsx:true
+  // },
 };
   
 export default nextConfig;
