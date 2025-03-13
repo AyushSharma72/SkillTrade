dd59d62a04cb3dea88e103e1142945ebc75afc0a
Ayush <asharma7588@gmail.com>
2025-03-13 18:16:57 +0530

    useclient

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,7 @@ import { AuthProvider } from "./_context/UserAuthContent";
 import GoogleTranslator from "./_components/GoogleTranslator";
 import Chatbot from "./_components/ChatBot";
 
-export const metadata = {
-  title: "Skill Trade",
-  description:
-    "A platform where individuals can book services for their house, hotel, or any place",
-};
+
 
 export default function RootLayout({ children }) {
   return (
