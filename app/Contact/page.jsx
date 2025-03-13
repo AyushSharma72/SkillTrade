6374c72d54834f176f0c5a686a514f1d0959b853
Ayush <asharma7588@gmail.com>
2025-03-13 17:56:20 +0530

    contact page

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,8 +1,10 @@
 "use client";
+export const dynamic = "force-dynamic"; // do not prerender this page
+
 import React, { useState } from "react";
-// import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
-// import { toast, Toaster } from "react-hot-toast";
-// import Image from "next/image";
+import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
+import { toast, Toaster } from "react-hot-toast";
+import Image from "next/image";
 import Footer from "../_components/Footer";
 
 const ContactForm = () => {
@@ -26,15 +28,15 @@ const ContactForm = () => {
         }
       );
       if (response.status === 200) {
-        // toast.success("We received your query");
+        toast.success("We received your query");
         SetName("");
         SetEmail("");
         SetMessage("");
       } else {
-        // toast.error("Please try after some time");
+        toast.error("Please try after some time");
       }
     } catch (error) {
-      // toast.error("Something went wrong");
+      toast.error("Something went wrong");
     } finally {
       Setloading(false);
     }
@@ -108,15 +110,17 @@ const Contact = () => {
   return (
     <>
       <div className="flex flex-col items-center w-full px-4 mt-8">
-        {/* <Toaster /> */}
+        <Toaster />
         <div className="flex flex-wrap justify-center gap-8 sm:w-3/4 p-3 bg-white shadow-lg mt-3">
           <ContactForm />
           <div className="flex flex-col p-3 rounded-lg">
-            {/* <Image
+            <Image
               src="/contacus.jpg"
               className="w-80 rounded-lg shadow-lg"
               alt="Contact Us"
-            /> */}
+              width={400}
+              height={200}
+            />
             <p className="text-xl font-semibold mt-4 mb-2 text-center">
               Contact us
             </p>
@@ -125,14 +129,14 @@ const Contact = () => {
                 href="mailto:asharma7588@gmail.com"
                 className=" text-lg flex items-center mt-2"
               >
-                {/* <FaEnvelope className="text-xl mr-2" /> asharma7588@gmail.com */}
+                <FaEnvelope className="text-xl mr-2" /> asharma7588@gmail.com
               </a>
 
               <a
                 href="mailto:Mohitsinghtadhiyal8@gmail.com"
                 className=" text-lg flex items-center mt-2"
               >
-                {/* <FaEnvelope className="text-xl mr-2" />{" "} */}
+                <FaEnvelope className="text-xl mr-2" />{" "}
                 Mohitsinghtadhiyal8@gmail.com
               </a>
 
@@ -142,7 +146,7 @@ const Contact = () => {
                   target="blank"
                   className="flex"
                 >
-                  {/* <FaLinkedin className="text-2xl mr-2" /> Ayush Sharma */}
+                  <FaLinkedin className="text-2xl mr-2" /> Ayush Sharma
                 </a>
               </div>
             </div>
@@ -153,11 +157,13 @@ const Contact = () => {
 
         <p className="text-2xl font-bold text-gray-800 mt-10">Developed By</p>
         <div className="flex flex-col items-center mt-4">
-          {/* <Image
+          <Image
             src="/Ayush2.jpg"
             alt="Ayush Sharma"
             className="w-32 h-32 rounded-full object-cover shadow-lg mb-4"
-          /> */}
+            width={200}
+            height={200}
+          />
           <p className="text-lg font-semibold">Ayush Sharma</p>
           <p className="text-gray-600">Developer and Project Manager</p>
           <div className="flex mt-4 space-x-4">
@@ -166,20 +172,20 @@ const Contact = () => {
               target="_blank"
               className="bg-gray-800 text-white p-3 rounded-full"
             >
-              {/* <FaGithub className="text-2xl" /> */}
+              <FaGithub className="text-2xl" />
             </a>
             <a
               href="mailto:asharma7588@gmail.com"
               className="bg-red-500 text-white p-3 rounded-full"
             >
-              {/* <FaEnvelope className="text-2xl" /> */}
+              <FaEnvelope className="text-2xl" />
             </a>
             <a
               href="https://www.linkedin.com/in/ayush-sharma-a155a8267"
               target="_blank"
               className="bg-blue-700 text-white p-3 rounded-full"
             >
-              {/* <FaLinkedin className="text-2xl" /> */}
+              <FaLinkedin className="text-2xl" />
             </a>
           </div>
         </div>
