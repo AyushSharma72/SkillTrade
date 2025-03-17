c676250590c82b4b44035afab904cadb779bf7e7
Ayush <asharma7588@gmail.com>
2025-03-17 21:39:24 +0530

    toast dynamic import

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,6 +1,4 @@
 "use client";
-export const dynamic = "force-dynamic"; // do not prerender this page
-
 import React, { useState } from "react";
 import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
 import dynamic from "next/dynamic";
